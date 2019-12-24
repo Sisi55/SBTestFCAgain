c694385d8ae22cce2278a8bbb3767977ee1c0597
Sisi <dqgsh1055@gmail.com>
2019-12-24 12:31:28 +0900

    Initial commit

---
+++ src/main/java/com/example/testFCagain/TestFCagainApplication.java
@@ -0,0 +1,13 @@
+package com.example.testFCagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFCagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFCagainApplication.class, args);
+	}
+
+}
